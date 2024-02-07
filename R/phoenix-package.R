8a114652b3897d5f3c8ab200de91349ce51763f5
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2024-02-07 16:17:47 -0700

    add package documentation file

---
+++ R/phoenix-package.R
@@ -0,0 +1,28 @@
+#' @title phoenix: Phoenix and Phoenix-8 Sepsis Criteria
+#'
+#' @description
+#' Implimentation of the Phoenix and Phoenix-8 Sepsis Criteria as described in
+#' "Development and Validation of the Phoenix Criteria for Pediatric Sepsis and
+#' Septic Shock" by Sanchez-Pinto, Bennett, DeWitt, Russell et.al. (2024)
+#' <doi:10.1001/jama.2024.0196> and "International Consensus Criteria for
+#' Pediatric Sepsis and Septic Shock" by Schlapback, Watson, Sorce, et.al.
+#' (2024) <doi:10.1001/jama.2024.0179>.
+#'
+#' @references
+#'
+#' Sanchez-Pinto LN, Bennett TD, DeWitt PE, et al. Development and Validation of
+#' the Phoenix Criteria for Pediatric Sepsis and Septic Shock. JAMA. Published
+#' online January 21, 2024. <doi:10.1001/jama.2024.0196>
+#'
+#' Schlapbach LJ, Watson RS, Sorce LR, et al. International Consensus Criteria
+#' for Pediatric Sepsis and Septic Shock. JAMA. Published online January 21,
+#' 2024. <doi:10.1001/jama.2024.0179>
+#'
+#' @docType package
+#' @keywords internal
+#' @aliases phoenix-package
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
