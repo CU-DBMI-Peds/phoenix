fc542b8374b27ee03b6e655c11e03ba990add438
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2025-04-30 14:30:48 -0600

    start adding the data_check method and supporting methods

--- R/data-quality-checks.R
+++ R/data-quality-checks.R
@@ -14,13 +14,241 @@
 #' @param fio2 numeric vector,  FiO2 is the fraction of inspired oxygen with
 #' expected values between 0.21 (room air) to 1.00.
 #'
-#' @name phoenix_input_data_checks
+#' @name data_checks
 NULL
 
 #'
+#' @rdname data_checks
 #' @export
-#phoenix_respiratory_data_checks(pf_ratio = NA_real_, sf_ratio = NA_real_, pao2 = NA_real_, spo2 = NA_real_, fio2 = NA_real_, imv = NA_integer_, other_respiratory_support = NA_integer_, data = parent.frame()) {
-#}
+data_checks <- function(
+    pf_ratio = NA_real_
+  , sf_ratio = NA_real_
+  , pao2 = NA_real_
+  , spo2 = NA_real_
+  , fio2 = NA_real_
+  , imv = NA_integer_
+  , other_respiratory_support = NA_integer_
+  , data = parent.frame()
+  ) {
+
+  pf_ratio <- eval(expr = substitute(pf_ratio), envir = data, enclos = parent.frame())
+  sf_ratio <- eval(expr = substitute(sf_ratio), envir = data, enclos = parent.frame())
+  pao2     <- eval(expr = substitute(pao2),     envir = data, enclos = parent.frame())
+  spo2     <- eval(expr = substitute(spo2),     envir = data, enclos = parent.frame())
+  fio2     <- eval(expr = substitute(fio2),     envir = data, enclos = parent.frame())
+  imv      <- eval(expr = substitute(imv),      envir = data, enclos = parent.frame())
+  other_respiratory_support  <- eval(expr = substitute(other_respiratory_support), envir = data, enclos = parent.frame())
+
+  length_check(pf_ratio = pf_ratio,
+               sf_ratio = sf_ratio,
+               pao2 = pao2,
+               spo2 = spo2,
+               fio2 = fio2,
+               imv = imv,
+               other_respiratory_support = other_respiratory_support)
+
+  # checks for mode
+  # recall that is.numeric will return TRUE for reals and integers
+  stopifnot(is.numeric(fio2))
+  stopifnot(is.numeric(spo2))
+  stopifnot(is.numeric(pao2))
+  stopifnot(is.numeric(pf_ratio))
+  stopifnot(is.numeric(sf_ratio))
+  stopifnot(is.numeric(imv))
+  stopifnot(is.numeric(other_respiratory_support))
+
+  ##############################################################################
+  # Tests to report on
+  tests <- list()
+  new_test <- function(test = "", warn_if = "", skip, pass = !fail, warn = NULL, fail = !pass) {
+    thistest <- list(list(test = test, warn_if = warn_if))
+    if (skip) {
+      thistest[[1]][["pass"]]    <- integer(0)
+      thistest[[1]][["warning"]] <- integer(0)
+      thistest[[1]][["fail"]]    <- integer(0)
+    } else {
+      thistest[[1]][["pass"]] <- which(pass)
+      thistest[[1]][["fail"]] <- which(!pass)
+      if (is.null(warn)) {
+        thistest[[1]][["warning"]] <- integer(0)
+      } else {
+        thistest[[1]][["warning"]] <- which(warn)
+      }
+    }
+    thistest[[1]][["skip"]] <- skip
+
+    assign("tests",
+           value = c(get("tests", envir = parent.frame()), thistest),
+           envir = parent.frame())
+  }
+
+  new_test(
+    test = "imv %in% c(0, 1)",
+    skip = all(is.na(imv)),
+    pass = is.na(imv) | (imv %in% c(0, 1))
+  )
+
+  new_test(
+    test = "other_respiratory_support %in% c(0, 1)",
+    skip = all(is.na(other_respiratory_support)),
+    pass = is.na(other_respiratory_support) | (other_respiratory_support %in% c(0, 1))
+  )
+
+  new_test(
+    test = "if imv == 1 then other_respiratory_support == 1",
+    skip = all(is.na(other_respiratory_support)) | all(is.na(imv)),
+    fail = (imv == 1) & (is.na(other_respiratory_support) | other_respiratory_support == 0)
+  )
+
+  new_test(
+    test = "0.21 <= fio2 <= 1.00",
+    skip = all(is.na(fio2)),
+    pass = is.na(fio2) | (fio2 >= 0.21 & fio2 <= 1.00)
+  )
+
+  new_test(
+    test    = "0 <= spo2 <= 100",
+    warn_if = "0 < spo2 < 1",
+    skip    = all(is.na(spo2)),
+    pass    = is.na(spo2) | (spo2 >= 0.00 & spo2 <= 100),
+    warn    = (spo2 > 0.00 & spo2 < 1.00)
+  )
+
+  new_test(
+    test = "0 <= sf_ratio <= 100/0.21",
+    skip = all(is.na(sf_ratio)),
+    pass = is.na(sf_ratio) | (sf_ratio >= 0.00 & sf_ratio <= 100/0.21)
+  )
+
+  new_test(
+    test    = "0 <= pao2", # technically there is no upper limit for pao2, but ther are unexpected values
+    warn_if = "pao2 > 100",
+    skip    = all(is.na(pao2)),
+    pass    = is.na(pao2) | (pao2 >= 0.00),
+    warn    = pao2 > 100
+  )
+
+  new_test(
+    test    = "0 <= pf_ratio",
+    warn_if = "pf_ratio > 100 / 0.21",
+    skip    = all(is.na(pf_ratio)),
+    pass    = is.na(pf_ratio) | (pf_ratio >= 0.00),
+    warn    = pf_ratio > 100 / 0.21
+  )
+
+  ## check for consistency between fio2 values and other respiratory support
+  new_test(
+    test = "(fio2 > 0.21) == other_respiratory_support",
+    skip = all(is.na(fio2)) | all(is.na(other_respiratory_support)),
+    pass = (as.integer(fio2 > 0.21) == other_respiratory_support)
+  )
+
+  ## check for the condition that sf_ratio is only used when spo2 is <= 97
+  new_test(
+    test    = "spo2 <= 97 | (spo2 > 97 & is.na(sf_ratio))",
+    warn_if = "spo2 > 97",
+    skip    = all(is.na(sf_ratio)) | all(is.na(spo2)),
+    pass    = is.na(sf_ratio) | is.na(spo2) | ((spo2 > 97) & is.na(sf_ratio)) | (spo2 <= 97),
+    warn    = spo2 > 97
+  )
+
+  ## check if the provided sf_ratio is equal to a calculated sf_ratio
+  new_test(
+    test = "spo2/fio2 == sf_ratio",
+    skip = all(is.na(sf_ratio)) | all(is.na(spo2)) | all(is.na(fio2)),
+    pass = is.na(spo2) | is.na(fio2) | is.na(sf_ratio) | ( (spo2 / fio2) > (sf_ratio - sqrt(.Machine$double.eps)) & (spo2 / fio2) < (sf_ratio + sqrt(.Machine$double.eps)))
+  )
+
+  ## check if the provided pf_ratio is equal to the calculated pf_ratio
+  new_test(
+    test = "pao2/fio2 == pf_ratio",
+    skip = all(is.na(pf_ratio)) | all(is.na(pao2)) | all(is.na(fio2)),
+    pass = is.na(pao2) | is.na(fio2) | is.na(pf_ratio) | ( (pao2 / fio2) > (pf_ratio - sqrt(.Machine$double.eps)) & (pao2 / fio2) < (pf_ratio + sqrt(.Machine$double.eps)))
+  )
+
+  tests[["considered_data"]] <-
+    data.frame(fio2, spo2, pao2, sf_ratio, pf_ratio, imv, other_respiratory_support)
+
+  class(tests) <- "phoenix_data_check"
+  tests
+}
+
+#' @rdname data_checks
+#' @export
+show_warnings <- function(x, test) {
+  UseMethod("show_warnings")
+}
+
+#' @rdname data_checks
+#' @export
+show_warnings.phoenix_data_check <- function(x, test) {
+  idx <- x[[test]][["warning"]]
+  x[["considered_data"]][idx, ]
+}
+
+#' @rdname data_checks
+#' @export
+show_fails <- function(x, test) {
+  UseMethod("show_fails")
+}
+
+#' @rdname data_checks
+#' @export
+show_fails.phoenix_data_check <- function(x, test) {
+  idx <- x[[test]][["fail"]]
+  x[["considered_data"]][idx, ]
+}
+
+#' @rdname data_checks
+#' @export
+report <- function(x, ...) {
+  UseMethod("report")
+}
+
+#' @rdname data_checks
+#' @export
+report.phoenix_data_check <- function(x, ...) {
+  DF <-
+    data.frame(test    = sapply(x[-length(x)], function(x) x[["test"]]),
+               warn_if = sapply(x[-length(x)], function(x) x[["warn_if"]]),
+               pass    = sapply(x[-length(x)], function(x) length(x[["pass"]])),
+               warning = sapply(x[-length(x)], function(x) length(x[["warning"]])),
+               fail    = sapply(x[-length(x)], function(x) length(x[["fail"]])),
+               skipped = sapply(x[-length(x)], function(x) x[["skip"]]),
+               row.names = NULL
+    )
+  class(DF) <- c("phoenix_data_check_report", class(DF))
+  DF
+}
+
+#' @export
+print.phoenix_data_check <- function(x, ...) {
+  str(x)
+}
+
+#' @export
+print.phoenix_data_check_report <- function(x, ...) {
+  cat("\nReport of the number of rows passing, failing, or with warning(s)\n\n")
+  NextMethod(x)
+
+  if (any(x[["warning"]] > 0) | any(x[["fail"]] > 0)) {
+
+    if (any(x[["warning"]] > 0)) {
+      warning("There is at least one test with warnings.")
+    }
+
+    if (any(x[["fail"]] > 0)) {
+      warning("There is at least one test with failings.")
+    }
+
+  } else if (all(x[["skipped"]])) {
+      warning("All tests were skipped")
+  } else {
+    message("No warnings.  No failures.")
+  }
+
+  invisible(x)
+}
 
 
 ################################################################################
---
+++ tests/test-respiratory-data-checks.R
@@ -0,0 +1,63 @@
+devtools::load_all('~/R-dev/phoenix')
+devtools::document("~/R-dev/phoenix")
+library(phoenix)
+set.seed(42)
+
+DATA <-
+  expand.grid(imv = c(NA_integer_, 0L, 1L, 2L),
+              other_respiratory_support = c(0L, 1L, 2L, NA_real_),
+              fio2 = c(seq(0.20, 1.00, by = 0.1), -1.0),
+              spo2 = c(seq(0, 100, by = 1), 0.88, 0.97, 0.98, -1, NA_real_),
+              pao2 = c(seq(0, 101, by = 1), -1, NA_real_)
+  )
+DATA[["sf_ratio"]] <- DATA[["spo2"]] / DATA[["fio2"]]
+DATA[["pf_ratio"]] <- DATA[["pao2"]] / DATA[["fio2"]]
+
+# add random noise to some of the sf_ratio and pf_ratio values
+idx <- sample(seq_len(nrow(DATA)), size = 1000)
+DATA[["sf_ratio"]][idx] <- DATA[["sf_ratio"]][idx] + rnorm(1000)
+
+idx <- sample(seq_len(nrow(DATA)), size = 1000)
+DATA[["pf_ratio"]][idx] <- DATA[["pf_ratio"]][idx] + rnorm(1000)
+
+# make sure there are cases where sf_ratio is provided when the fio2 and/or spo2
+# is not provide
+U   <- with(DATA, unique(spo2/fio2))
+N   <- length(U)
+idx <- which(is.na(DATA[["fio2"]]) | is.na(DATA[["spo2"]]))
+idx <- sample(idx, size = N)
+DATA[["sf_ratio"]][idx] <- sample(U)
+
+# same for pf_ratio
+U   <- with(DATA, unique(pao2/fio2))
+N   <- length(U)
+idx <- which(is.na(DATA[["fio2"]]) | is.na(DATA[["pao2"]]))
+idx <- sample(idx, size = N)
+DATA[["pf_ratio"]][idx] <- sample(U)
+
+temp <-
+data_checks(imv = imv,
+                                other_respiratory_support = other_respiratory_support,
+                                fio2 = fio2,
+                                spo2 = spo2,
+                                pao2 = pao2,
+                                sf_ratio = sf_ratio,
+                                pf_ratio = pf_ratio,
+                                data = DATA)
+#lapply(temp, names)
+#temp# |> print()
+#temp
+#report(temp)
+#show_warnings(temp, test = 4) |> head()
+#with(show_fails(temp, test = 1), table(imv))
+
+temp <-
+data_checks(imv = vent,
+                                other_respiratory_support = as.integer(fio2 > 0.21),
+                                fio2 = fio2,
+                                spo2 = spo2,
+                                pao2 = pao2,
+                                data = sepsis)
+temp |> report()
+#show_fails(temp, 3)
+
