3dce5a888f67c27acd343561e30b72e2a32a8770
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2026-04-07 14:54:56 -0600

    improve documentation

--- R/operationalize.R
+++ R/operationalize.R
@@ -1,5 +1,5 @@
-#' Prepare Observation, Interventions, Event, Medications, Tests for Phoenix
-#' Scoring
+#' Prepare Continuous Observation, Intervention, Medication, and Test Inputs for
+#' Phoenix Scoring
 #'
 #' Functions for checking the basic structure and values for the input data for
 #' Assessing Phoenix.
@@ -9,10 +9,8 @@
 #' amount of time from admission, \code{eclock}
 #' ('encounter clock'; generally expected to be in minutes with 0 being the encounter start).
 #' \code{value.var} denotes the reported values for the input of interest.
-#' \code{valid.range} and \code{valid.values} are used for simple checks for
-#' valid values.  Only one of the two can be specified.  The checks are
-#' \code{x[[value.var]] >= min(valid.range) & x[[value.var]] <= max(valid.range)}
-#' or \code{x[[value.var]] \%in\% valid.values}.
+#' \code{valid.range} is used for simple checks for valid values via
+#' \code{x[[value.var]] >= min(valid.range) & x[[value.var]] <= max(valid.range)}.
 #'
 #' There is an expectation when going to Phoenix scoring that the
 #' \code{x[[c(id.vars, eclock)]]} are unique for each input.  These functions
@@ -42,23 +40,17 @@
 #' @param valid.range A numeric vector of length two defining an interval of
 #' valid values for \code{x[[value.var]]}. The defaults are set to be
 #' considerably wider than clinically possible in some cases, e.g., infinite
-#' upper limit for blood pressures.  Only one of \code{valid.range} and
-#' \code{valid.values} is allowed to be non-NULL.
-#'
-#' @param valid.values A set of values which \code{x[[value.var]]} can take on,
-#' example, indicators are checked againt \code{c(0, 1)} and GCS Total is
-#' checked against 3:15. Only one of \code{valid.range} and \code{valid.values}
-#' is allowed to be non-NULL.
+#' upper limit for blood pressures.
 #'
 #' @param tie.breaker When \code{x[c(id.vars, eclock)]} is not unique this
 #' function is uses to aggregate \code{x[[value.var]]} into one value.
 #'
 #' @param verbose when \code{TRUE} print messages showing the progress
 #'
-#' @name prepare_inputs
+#' @name prepare_inputs_range
 NULL
 
-#' @rdname prepare_inputs
+#' @rdname prepare_inputs_range
 #' @export
 prepare_fio2 <-
   function(
@@ -79,7 +71,7 @@ prepare_fio2 <-
   rtn
 }
 
-#' @rdname prepare_inputs
+#' @rdname prepare_inputs_range
 #' @export
 prepare_spo2 <-
   function(
@@ -100,7 +92,7 @@ prepare_spo2 <-
   rtn
 }
 
-#' @rdname prepare_inputs
+#' @rdname prepare_inputs_range
 #' @export
 prepare_pao2 <-
   function(
@@ -121,7 +113,7 @@ prepare_pao2 <-
   rtn
 }
 
-#' @rdname prepare_inputs
+#' @rdname prepare_inputs_range
 #' @export
 prepare_vent <-
   function(
@@ -141,7 +133,7 @@ prepare_vent <-
   rtn
 }
 
-#' @rdname prepare_inputs
+#' @rdname prepare_inputs_range
 #' @export
 prepare_hfov <-
   function(
@@ -161,7 +153,7 @@ prepare_hfov <-
   rtn
 }
 
-#' @rdname prepare_inputs
+#' @rdname prepare_inputs_range
 #' @export
 prepare_peep <-
   function(
@@ -181,7 +173,57 @@ prepare_peep <-
   rtn
 }
 
-#' @rdname prepare_inputs
+#' Prepare Discrete Observation, Intervention, Event, and Test Inputs for
+#' Phoenix Scoring
+#'
+#' Functions for checking the basic structure and values for Phoenix inputs with
+#' fixed allowable values.
+#'
+#' The input data is expected to be in a "long" format with \code{id.vars}
+#' (examples: hospital id, patient id, encounter id). A column for reporting the
+#' amount of time from admission, \code{eclock}
+#' ('encounter clock'; generally expected to be in minutes with 0 being the encounter start).
+#' \code{value.var} denotes the reported values for the input of interest.
+#'
+#' These functions validate against input-specific fixed allowable values such
+#' as \code{c(0, 1)} for indicators or the known score sets for GCS
+#' components/subscores. End users are not expected to change those allowable
+#' values.
+#'
+#' There is an expectation when going to Phoenix scoring that the
+#' \code{x[[c(id.vars, eclock)]]} are unique for each input.  These functions
+#' check this assumption and will aggregate, if needed, using the
+#' \code{tie.breaker} method.
+#'
+#' Default values are based on the values used when building the Phoenix
+#' criteria, see Sanchez-Pinto, Bennett, DeWitt, Russell, et al. (2024).
+#'
+#' @references See reference details in \code{\link{phoenix-package}} or by calling
+#' \code{citation('phoenix')}.
+#'
+#' @param x a data.frame, or object that inherits from a data.frame such as
+#' data.table or tibble.
+#'
+#' @param id.vars a character vector, expected to be a at least length 1, of the
+#' names of the columns of \code{x} to be used to identifiers, e.g., hospital
+#' id, patient id, encounter id.
+#'
+#' @param eclock A character vector of length 1, the name of the column in
+#' \code{x} denoting the time, in minutes, from admission start.
+#'
+#' @param value.var A character vector of length 1, the name of the column in
+#' \code{x} containing the value for the observation, intervention, event,
+#' medicaiton, or test.
+#'
+#' @param tie.breaker When \code{x[c(id.vars, eclock)]} is not unique this
+#' function is uses to aggregate \code{x[[value.var]]} into one value.
+#'
+#' @param verbose when \code{TRUE} print messages showing the progress
+#'
+#' @name prepare_inputs_discrete
+NULL
+
+#' @rdname prepare_inputs_discrete
 #' @export
 prepare_imv <-
   function(
@@ -201,7 +243,7 @@ prepare_imv <-
   rtn
 }
 
-#' @rdname prepare_inputs
+#' @rdname prepare_inputs_discrete
 #' @export
 prepare_o2support <-
   function(
@@ -221,7 +263,7 @@ prepare_o2support <-
   rtn
 }
 
-#' @rdname prepare_inputs
+#' @rdname prepare_inputs_discrete
 #' @export
 prepare_dobutamine <-
   function(
@@ -241,7 +283,7 @@ prepare_dobutamine <-
   rtn
 }
 
-#' @rdname prepare_inputs
+#' @rdname prepare_inputs_discrete
 #' @export
 prepare_dopamine <-
   function(
@@ -261,7 +303,7 @@ prepare_dopamine <-
   rtn
 }
 
-#' @rdname prepare_inputs
+#' @rdname prepare_inputs_discrete
 #' @export
 prepare_epinephrine <-
   function(
@@ -281,7 +323,7 @@ prepare_epinephrine <-
   rtn
 }
 
-#' @rdname prepare_inputs
+#' @rdname prepare_inputs_discrete
 #' @export
 prepare_milrinone <-
   function(
@@ -301,7 +343,7 @@ prepare_milrinone <-
   rtn
 }
 
-#' @rdname prepare_inputs
+#' @rdname prepare_inputs_discrete
 #' @export
 prepare_norepinephrine <-
   function(
@@ -321,7 +363,7 @@ prepare_norepinephrine <-
   rtn
 }
 
-#' @rdname prepare_inputs
+#' @rdname prepare_inputs_discrete
 #' @export
 prepare_vasopressin <-
   function(
@@ -341,7 +383,7 @@ prepare_vasopressin <-
   rtn
 }
 
-#' @rdname prepare_inputs
+#' @rdname prepare_inputs_range
 #' @export
 prepare_lactate <-
   function(
@@ -361,7 +403,7 @@ prepare_lactate <-
   rtn
 }
 
-#' @rdname prepare_inputs
+#' @rdname prepare_inputs_range
 #' @export
 prepare_sbpc <-
   function(
@@ -381,7 +423,7 @@ prepare_sbpc <-
   rtn
 }
 
-#' @rdname prepare_inputs
+#' @rdname prepare_inputs_range
 #' @export
 prepare_sbpa <-
   function(
@@ -401,7 +443,7 @@ prepare_sbpa <-
   rtn
 }
 
-#' @rdname prepare_inputs
+#' @rdname prepare_inputs_range
 #' @export
 prepare_dbpc <-
   function(
@@ -421,7 +463,7 @@ prepare_dbpc <-
   rtn
 }
 
-#' @rdname prepare_inputs
+#' @rdname prepare_inputs_range
 #' @export
 prepare_dbpa <-
   function(
@@ -441,7 +483,7 @@ prepare_dbpa <-
   rtn
 }
 
-#' @rdname prepare_inputs
+#' @rdname prepare_inputs_range
 #' @export
 prepare_mapc <-
   function(
@@ -461,7 +503,7 @@ prepare_mapc <-
   rtn
 }
 
-#' @rdname prepare_inputs
+#' @rdname prepare_inputs_range
 #' @export
 prepare_mapa <-
   function(
@@ -481,7 +523,7 @@ prepare_mapa <-
   rtn
 }
 
-#' @rdname prepare_inputs
+#' @rdname prepare_inputs_discrete
 #' @export
 prepare_gcseye <-
   function(
@@ -501,7 +543,7 @@ prepare_gcseye <-
   rtn
 }
 
-#' @rdname prepare_inputs
+#' @rdname prepare_inputs_discrete
 #' @export
 prepare_gcsmotor <-
   function(
@@ -521,7 +563,7 @@ prepare_gcsmotor <-
   rtn
 }
 
-#' @rdname prepare_inputs
+#' @rdname prepare_inputs_discrete
 #' @export
 prepare_gcsverbal <-
   function(
@@ -541,7 +583,7 @@ prepare_gcsverbal <-
   rtn
 }
 
-#' @rdname prepare_inputs
+#' @rdname prepare_inputs_discrete
 #' @export
 prepare_gcstotal <-
   function(
@@ -561,7 +603,7 @@ prepare_gcstotal <-
   rtn
 }
 
-#' @rdname prepare_inputs
+#' @rdname prepare_inputs_discrete
 #' @export
 prepare_pupill <-
   function(
@@ -581,7 +623,7 @@ prepare_pupill <-
   rtn
 }
 
-#' @rdname prepare_inputs
+#' @rdname prepare_inputs_discrete
 #' @export
 prepare_pupilr <-
   function(
@@ -601,7 +643,7 @@ prepare_pupilr <-
   rtn
 }
 
-#' @rdname prepare_inputs
+#' @rdname prepare_inputs_discrete
 #' @export
 prepare_pupils <-
   function(
@@ -621,7 +663,7 @@ prepare_pupils <-
   rtn
 }
 
-#' @rdname prepare_inputs
+#' @rdname prepare_inputs_range
 #' @export
 prepare_platelets <-
   function(
@@ -641,7 +683,7 @@ prepare_platelets <-
   rtn
 }
 
-#' @rdname prepare_inputs
+#' @rdname prepare_inputs_range
 #' @export
 prepare_inr <-
   function(
@@ -661,7 +703,7 @@ prepare_inr <-
   rtn
 }
 
-#' @rdname prepare_inputs
+#' @rdname prepare_inputs_range
 #' @export
 prepare_ddimer <-
   function(
@@ -681,7 +723,7 @@ prepare_ddimer <-
   rtn
 }
 
-#' @rdname prepare_inputs
+#' @rdname prepare_inputs_range
 #' @export
 prepare_fibrinogen <-
   function(
