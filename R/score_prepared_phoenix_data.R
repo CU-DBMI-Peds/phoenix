f8205e0e3fea438cd6e57bfaadd8c0a5d07a2cbd
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2026-04-09 16:12:11 -0600

    wip

--- R/score_prepared_phoenix_data.R
+++ R/score_prepared_phoenix_data.R
@@ -10,26 +10,78 @@
 #'   See Details.
 #'
 #' @export
-score_prepared_phoenix_data <- function(x, T0 = 0, T1 = 1440, sigma = 2, kappa = 1, version = c("jama2024", "alt1", "alt2")) {
+score_prepared_phoenix_data <- function(x, T0 = 0, T1 = 1440, sigma = 2, kappa = 1, version = c("jama2024", "alt1", "alt2"), verbose = getOption("phoenix_verbose", interactive())) {
   stopifnot(inherits(x, "prepared_phoenix_data"))
   version <- match.arg(version, several.ok = FALSE)
 
-  rtn <-
+  if (verbose) message("Scoring prepared_phoenix_data...")
+  if (verbose) message("  identifying suspsected infections...")
+  # suspected infection is set for the id.var over the window of interest
+  si <-
+    phxdft_set(
+      x = phxdft_select(x, c(attr(x, "id.vars"), attr(x, "eclock"))),
+      j = "suspected_infection",
+      value =
+        as.integer(
+          !is.na(x[["ANTIMICROBIALS"]])     &
+          !is.na(x[["ANTIINFECTIOUSTESTS"]]) &
+          (x[["ANTIMICROBIALS"]] == 1)       &
+          (x[["ANTIINFECTIOUSTESTS"]] == 1)
+        )
+    )
+
+  # aggregate suspected infections to a 0/1 for the window of interest
+  si <-
+    phxdft_subset(
+      x = si,
+      i = which((si[[attr(x, "eclock")]] >= T0) & (si[[attr(x, "eclock")]] < T1))
+    )
+
+  si <-
+    aggregate(
+      x  = phxdft_select(si, "suspected_infection"),
+      by = phxdft_select(si, attr(x, "id.vars")),
+      FUN = max
+    )
+
+  if (verbose) message("  identifying records to score...")
+  # now, the data of interest that needs to be scored is only for those with a
+  # suspected infection and within the window [T0, T1)
+  score_this <-
+    phxdft_inner_join(
+      x = x,
+      y = phxdft_subset(si, i = which(si[["suspected_infection"]] > 0), cols = attr(x, "id.vars")),
+      by = attr(x, "id.vars")
+    )
+
+  if (verbose) message("  applying scoring...")
+  # apply the scoring method to the oss and si
+  pss <-
     switch(
       version,
-      jama2024 = jama2024(x, T0, T1, sigma = sigma, kappa = kappa),
-      alt1     = alt1(x, T0, T1, sigma = sigma, kappa = kappa)
+      jama2024 = jama2024(x = score_this, id.vars = attr(x, "id.vars"), eclock = attr(x, "eclock"), sigma = sigma, kappa = kappa, verbose = verbose),
+      alt1     = alt1(    x = score_this, id.vars = attr(x, "id.vars"), eclock = attr(x, "eclock"), sigma = sigma, kappa = kappa, verbose = verbose)
     )
+
+  if (verbose) message("  building outout...")
+  iddf <- phxdft_unique(phxdft_select(x, attr(x, "id.vars")))
+  rtn <- phxdft_left_join(iddf, si, attr(x, "id.vars"))
+  rtn <- phxdft_left_join(rtn, pss, attr(x, "id.vars"))
+
   attr(rtn, "T0") <- T0
   attr(rtn, "T1") <- T1
   attr(rtn, "version") <- version
   class(rtn) <- c("scored_prepared_phoenix_data", class(rtn))
+
+  if (verbose) message("Scoring complete!")
   rtn
 }
 
-jama2024 <- function(x, T0, T1, sigma, kappa) {
+jama2024 <- function(x, id.vars, eclock, sigma, kappa, verbose) {
+  # Overlly simplified, the score is max( resp + card + neuro + coag )
 
-  # organ system scores
+  if (verbose) message("    building organ system scores...")
+  # find all the needed organ system scores at every moment in time
   respscore <-
     phoenix_respiratory(
       pf_ratio = x[["PFR"]],
@@ -83,207 +135,201 @@ jama2024 <- function(x, T0, T1, sigma, kappa) {
       alt = x[["ALT"]]
     )
 
-  scores <- phxdft_select(x, c(attr(x, "id.vars"), attr(x, "eclock")))
+  oss <- phxdft_select(x, c(id.vars, eclock))
+  oss <- phxdft_set(oss, j = "respscore", value = respscore)
+  oss <- phxdft_set(oss, j = "cardscore", value = cardscore)
+  oss <- phxdft_set(oss, j = "neuroscore", value = neuroscore)
+  oss <- phxdft_set(oss, j = "coagscore", value = coagscore)
+  oss <- phxdft_set(oss, j = "endoscore", value = endoscore)
+  oss <- phxdft_set(oss, j = "immunscore", value = immunscore)
+  oss <- phxdft_set(oss, j = "hepaticscore", value = hepaticscore)
+  oss <- phxdft_set(oss, j = "renalscore", value = renalscore)
 
-  scores <-
+  oss <-
     phxdft_set(
-      x = scores,
+      x = oss,
       j = "phoenix_sepsis_score",
       value = respscore + cardscore + neuroscore + coagscore
     )
-  scores <-
+
+  oss <-
     phxdft_set(
-      x = scores,
+      x = oss,
       j = "phoenix_septic_shock_score",
-      value = as.integer(cardscore >= kappa) * scores[["phoenix_sepsis_score"]]
+      value = as.integer(cardscore >= kappa) * oss[["phoenix_sepsis_score"]]
     )
-  scores <-
+
+  oss <-
     phxdft_set(
-      x = scores,
+      x = oss,
       j = "phoenix8_sepsis_score",
       value = respscore + cardscore + neuroscore + coagscore + 
               immunscore + endoscore + renalscore + hepaticscore
     )
 
-  # suspected infection
-  si <-
-    phxdft_set(
-      x = phxdft_select(x, c(attr(x, "id.vars"), attr(x, "eclock"))),
-      j = "SI",
-      value =
-        as.integer(
-          !is.na(x[["ANTIMICROBIALS"]]) & !is.na(x[["ANTIINFECTIOUSTESTS"]]) &
-            (x[["ANTIMICROBIALS"]] == 1) & (x[["ANTIINFECTIOUSTESTS"]] == 1)
-        )
-    )
-
-  # aggregate suspected infections to a 0/1 for the window of interest
-  si <-
-    phxdft_subset(
-      x = si,
-      i = which((si[[attr(x, "eclock")]] >= T0) & (si[[attr(x, "eclock")]] < T1))
-    )
-  si <-
+  if (verbose) message("    aggregating....")
+  # find the max value of the scores
+  oss <-
     aggregate(
-      x = phxdft_select(si, "SI"),
-      by = phxdft_select(si, id.vars),
+      x = phxdft_select(oss, c("phoenix_sepsis_score", "phoenix_septic_shock_score", "phoenix8_sepsis_score")),
+      by = phxdft_select(oss, id.vars),
       FUN = max
     )
 
-  # subset scores to the window of interest, and merge on the SI value
-  scores <-
-    phxdft_subset(
-      x = scores,
-      i = which((scores[[attr(x, "eclock")]] >= T0) & (scores[[attr(x, "eclock")]] < T1))
-    )
-
-  scores <- phxdft_left_join(scores, si, by = attr(x, 'id.vars'))
-
-  # scores multipled by SI to zero out values for those without a
-  # suspected infection
-  scores <-
+  if (verbose) message("    building indicators....")
+  # build the sepsis and septic shock indicators
+  oss <-
     phxdft_set(
-      x = scores,
-      j = "phoenix_sepsis_score",
-      value = scores[["SI"]] * scores[["phoenix_sepsis_score"]]
-    )
-
-  scores <-
-    phxdft_set(
-      x = scores,
-      j = "phoenix_septic_shock_score",
-      value = scores[["SI"]] * scores[["phoenix_septic_shock_score"]]
-    )
-
-  scores <-
-    phxdft_set(
-      x = scores,
-      j = "phoenix8_sepsis_score",
-      value = scores[["SI"]] * scores[["phoenix8_sepsis_score"]]
-    )
-
-  # find the max value
-  scores <-
-    aggregate(
-      x = phxdft_select(scores, c("SI", "phoenix_sepsis_score", "phoenix_septic_shock_score", "phoenix8_sepsis_score")),
-      by = phxdft_select(scores, id.vars),
-      FUN = max
-    )
-
-  scores <-
-    phxdft_setnames(
-      x = scores,
-      old = "SI",
-      new = "suspected_infection"
-    )
-
-  scores <-
-    phxdft_set(
-      x = scores,
+      x = oss,
       j = "phoenix_sepsis",
-      value = as.integer(scores[["phoenix_sepsis_score"]] >= sigma)
+      value = as.integer(oss[["phoenix_sepsis_score"]] >= sigma)
     )
 
-  scores <-
+  oss <-
     phxdft_set(
-      x = scores,
+      x = oss,
       j = "phoenix_septic_shock",
-      value = as.integer(scores[["phoenix_septic_shock_score"]] >= sigma)
+      value = as.integer(oss[["phoenix_septic_shock_score"]] >= sigma)
     )
 
-  scores <-
+  # omit the septic_shock_score
+  oss <-
     phxdft_set(
-      x = scores,
+      x = oss,
       j = "phoenix_septic_shock_score",
       value = NULL
     )
 
-  scores
+  oss
 
 }
 
-alt1 <- function(x, T0, T1) {
-  scores <-
-    phoenix8(
-      pf_ratio = PFR,
-      sf_ratio = SFR,
-      imv = IMV,
-      other_respiratory_support = ORS,
-      vasoactives = DOBUTAMINE + DOPAMINE + EPINEPHRINE + MILRINONE + NOREPINEPHRINE + VASOPRESSIN,
-      lactate = LACTATE,
-      map = MAP,
-      platelets = PLATELETS,
-      inr = INR,
-      d_dimer = DDIMER,
-      fibrinogen = FIBRINOGEN,
-      gcs = GCS,
-      fixed_pupils = FIXEDPUPILS,
-      glucose = GLUCOSE,
-      anc = ANC,
-      alc = ALC,
-      creatinine = CREATININE,
-      bilirubin = BILIRUBIN,
-      alt = ALT,
-      age = AGE,
-      data = x
-    )
-  scores <-
-    cbind(
-      phxdft_select(x, c(attr(x, "id.vars"), attr(x, "eclock"))),
-      scores
+alt1 <- function(x, id.vars, eclock, sigma, kappa, verbose) {
+  # Overlly simplified, the score is 
+  # max(resp) + max(card) + max(neuro) + max(coag)
+
+  if (verbose) message("    building organ system scores...")
+  # find all the needed organ system scores at every moment in time
+  respscore <-
+    phoenix_respiratory(
+      pf_ratio = x[["PFR"]],
+      sf_ratio = x[["SFR"]],
+      imv = x[["IMV"]],
+      other_respiratory_support = x[["ORS"]]
     )
 
-  keep <- (scores[[attr(x, "eclock")]] >= T0) & (scores[[attr(x, "eclock")]] < T1)
-  scores <- phxdft_subset(scores, i = keep)
+  cardscore <-
+    phoenix_cardiovascular(
+      vasoactives = x[["DOBUTAMINE"]] + x[["DOPAMINE"]] + x[["EPINEPHRINE"]] + x[["MILRINONE"]] + x[["NOREPINEPHRINE"]] + x[["VASOPRESSIN"]],
+      lactate = x[["LACTATE"]],
+      map = x[["MAP"]],
+      age = x[["AGE"]],
+    )
 
-  scores <-
+  coagscore <-
+    phoenix_coagulation(
+      platelets = x[["PLATELETS"]],
+      inr = x[["INR"]],
+      d_dimer = x[["DDIMER"]],
+      fibrinogen = x[["FIBRINOGEN"]]
+    )
+
+  neuroscore <-
+    phoenix_neurologic(
+      gcs = x[["GCS"]],
+      fixed_pupils = x[["FIXEDPUPILS"]]
+    )
+
+  endoscore <-
+    phoenix_endocrine(
+      glucose = x[["GLUCOSE"]]
+    )
+
+  immunscore <-
+    phoenix_immunologic(
+      anc = x[["ANC"]],
+      alc = x[["ALC"]]
+    )
+
+  renalscore <-
+    phoenix_renal(
+      creatinine = x[["CREATININE"]],
+      age = x[["AGE"]]
+    )
+
+  hepaticscore <-
+    phoenix_hepatic(
+      bilirubin = x[["BILIRUBIN"]],
+      alt = x[["ALT"]]
+    )
+
+  oss <- phxdft_select(x, c(id.vars, eclock))
+  oss <- phxdft_set(oss, j = "respscore", value = respscore)
+  oss <- phxdft_set(oss, j = "cardscore", value = cardscore)
+  oss <- phxdft_set(oss, j = "neuroscore", value = neuroscore)
+  oss <- phxdft_set(oss, j = "coagscore", value = coagscore)
+  oss <- phxdft_set(oss, j = "endoscore", value = endoscore)
+  oss <- phxdft_set(oss, j = "immunscore", value = immunscore)
+  oss <- phxdft_set(oss, j = "hepaticscore", value = hepaticscore)
+  oss <- phxdft_set(oss, j = "renalscore", value = renalscore)
+
+
+  if (verbose) message("    aggregating....")
+  oss <-
     aggregate(
-      x = 
-        phxdft_select(scores, 
-          paste0(
-            "phoenix_",
-            c("respiratory", "cardiovascular", "coagulation", "neurologic", "endocrine", "immunologic", "hepatic", "renal"),
-            "_score")
-        )
-      ,
-      by  = phxdft_select(scores, attr(x, "id.vars")),
+      x = phxdft_select(oss, c("respscore", "cardscore", "neuroscore", "coagscore", "endoscore", "immunscore", "hepaticscore", "renalscore")),
+      by = phxdft_select(oss, id.vars),
       FUN = max
     )
 
-  #
-  scores <-
+  if (verbose) message("    building indicators....")
+  oss <-
     phxdft_set(
-      x = scores,
+      x = oss,
       j = "alt1_sepsis_score",
-      value = scores[["phoenix_respiratory_score"]] +
-              scores[["phoenix_cardiovascular_score"]] +
-              scores[["phoenix_neurologic_score"]] +
-              scores[["phoenix_coagulation_score"]]
-    )
-  scores <-
-    phxdft_set(
-      x = scores,
-      j = "alt1_8_sepsis_score",
-      value = 
-        scores[["alt1_sepsis_score"]] +
-        scores[["phoenix_immunologic_score"]] +
-        scores[["phoenix_endocrine_score"]] +
-        scores[["phoenix_hepatic_score"]] +
-        scores[["phoenix_renal_score"]]
-    )
-  scores <-
-    phxdft_set(
-      x = scores,
-      j = "alt1_sepsis",
-      value = as.integer(scores[["alt1_sepsis_score"]] >= 2)
-    )
-  scores <-
-    phxdft_set(
-      x = scores,
-      j = "alt1_septic_shock",
-      value = as.integer(scores[["phoenix_cardiovascular_score"]] > 0 & scores[["alt1_sepsis_score"]] >= 2)
+      value = oss[["respscore"]] +
+              oss[["cardscore"]] +
+              oss[["neuroscore"]] +
+              oss[["coagscore"]]
     )
 
-  phxdft_select(scores, c(id.vars, grep("^alt1", names(scores), value = TRUE)))
+  oss <-
+    phxdft_set(
+      x = oss,
+      j = "alt1_septic_shock_score",
+      value = as.integer(oss[["cardscore"]] >= kappa) * oss[["alt1_sepsis_score"]]
+    )
+
+  oss <-
+    phxdft_set(
+      x = oss,
+      j = "alt1_8_sepsis_score",
+      value = oss[["respscore"]]  + oss[["cardscore"]] + oss[["neuroscore"]] + oss[["coagscore"]] + 
+              oss[["immunscore"]] + oss[["endoscore"]] + oss[["renalscore"]] + oss[["hepaticscore"]]
+    )
+
+  oss <-
+    phxdft_set(
+      x = oss,
+      j = "alt1_sepsis",
+      value = as.integer(oss[["alt1_sepsis_score"]] >= sigma)
+    )
+
+  oss <-
+    phxdft_set(
+      x = oss,
+      j = "alt1_septic_shock",
+      value = as.integer(oss[["alt1_septic_shock_score"]] >= sigma)
+    )
+
+  # omit the septic_shock_score
+  oss <-
+    phxdft_set(
+      x = oss,
+      j = "alt1_septic_shock_score",
+      value = NULL
+    )
+
+  oss
 
 }
